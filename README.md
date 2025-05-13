6890f175b1260526efe64dfea4a5b0f9dff41389
AlanJ2006 <alanwilliamsjimenezbolado@gmail.com>
2025-05-12 21:24:01 -0400

    Este commit agrega los archivos README.md, informeprimelist.pdf y primelist.py, este es el primer commit del repositorio

---
+++ README.md
@@ -0,0 +1,5 @@
+|Nombres     |Usuarios GitHub|Correos                       |
+|------------|---------------|------------------------------|
+|Alan Jimenez|AlanJ2006      |alan.jimenezb@estudiante.uc.cl|
+
+**Alan Jimenez**: Todos los elementos presentados
---
+++ primelist.py
@@ -0,0 +1,15 @@
+def primelist(number):
+    if number < 2:
+        return []
+    numlist = list(range(2, number+1))
+    primelist = []
+    for i in range(2, number+1):
+        if i in numlist:
+            primelist.append(i)
+        for x in range(i, number+1, i):
+            if x in numlist:
+                numlist.remove(x)
+    return primelist
+
+n = int(input())
+print(primelist(n))
